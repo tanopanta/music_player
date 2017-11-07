8929e1e5436937bb366e1c63abb3db02280f258b
tanopanta <rainstorm2525@gmail.com>
2017-11-07 19:53:12 +0900

    [add]ポーズを追加。そのため全体をスレッド化

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import glob
 import os
 import sys
@@ -15,6 +16,9 @@ class Player:
         print(self.mp3_list)
 
         mixer.init()
+
+        self.in_pause = False
+
         print("ctrl+c stop")
     
     def loop(self):
@@ -22,13 +26,19 @@ class Player:
             mixer.music.load(mp3)
             mixer.music.play() 
             print(os.path.splitext(os.path.basename(mp3))[0])
-            while mixer.music.get_busy(): 
+            while mixer.music.get_busy() or self.in_pause:
                 time.sleep(1)
     
     def skip(self):
         mixer.music.stop()
-
     
+    def pause(self):
+        self.in_pause = True
+        mixer.music.pause()
+    
+    def unpause(self):
+        self.in_pause = False
+        mixer.music.unpause()
 
     def end(self):
         print("end")
@@ -37,10 +47,24 @@ class Player:
 
 def main():
     player = Player() 
-    try:
-        player.loop()
-    except KeyboardInterrupt:
-        player.end()
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    executor.submit(player.loop)
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            s = input("入力 skip, end, pause, unpause:")
+            if s == "skip":
+                player.skip()
+            elif s == "end":
+                player.end()
+            elif s == "pause":
+                player.pause()
+            elif s == "unpause":
+                player.unpause()
+            else:
+                print("キーエラー。終了します")
+                player.end()
     
 
 if __name__ == '__main__':
